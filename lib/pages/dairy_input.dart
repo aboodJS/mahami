6da25cb2f5936caa887957b9d0a36611907275aa
ajs <140010746+aboodJS@users.noreply.github.com>
2026-08-12 00:10:11 +0300

    started working on an input page for the dairy

---
+++ lib/pages/dairy_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DairyInput extends StatefulWidget {
+  const DairyInput({super.key});
+
+  @override
+  State<DairyInput> createState() => _DairyInputState();
+}
+
+class _DairyInputState extends State<DairyInput> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
